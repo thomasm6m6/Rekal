30cf721d7c5b96e5ef4c7152244a5fb6eaa1149f
Thomas M <thomasm6m6@gmail.com>
2024-12-30 02:09:37 -0800

    Make process.swift work as a daemon

--- process.swift
+++ process.swift
@@ -1,6 +1,7 @@
 import Foundation
 import VisionKit
 import AppKit // included for NSImage. alternative? or is this the correct approach?
+import IOKit.ps
 
 // FIXME figure out why there are discrepancies between the file index as calculated by this implementation vs process.py
 
@@ -8,6 +9,9 @@ let rootDir = FileManager.default.homeDirectoryForCurrentUser.appendingPathCompo
 let imgDir = rootDir.appendingPathComponent("img")
 let fullDir = rootDir.appendingPathComponent("full")
 let smallDir = rootDir.appendingPathComponent("small")
+
+var isOnPower = false
+var isProcessing = false
 
 func performOCR(on imageURL: URL) async -> String {
     // let imageURL = URL(fileURLWithPath: imagePath)
@@ -36,7 +40,8 @@ func ocr(filePath: URL) async throws {
     try result.write(to: ocrPath, atomically: true, encoding: .utf8)
 }
 
-func encode(blockLength: TimeInterval) throws -> [Int: [URL]] {
+func encode() throws -> [Int: [URL]] {
+    print("encode()")
     var files: [Int: [URL]] = [:]
     let now = Int(Date().timeIntervalSince1970)
 
@@ -55,11 +60,11 @@ func encode(blockLength: TimeInterval) throws -> [Int: [URL]] {
         guard let timestamp = Int(dirURL.lastPathComponent) else {
             continue
         }
-        if now - timestamp < Int(blockLength) {
+        if now - timestamp < 300 {
             continue
         }
 
-        let block = (timestamp / Int(blockLength)) * Int(blockLength)
+        let block = timestamp / 300 * 300
         if files[block] != nil {
             files[block]?.append(dirURL)
         } else {
@@ -73,13 +78,47 @@ func encode(blockLength: TimeInterval) throws -> [Int: [URL]] {
         }
     }
 
+    print("end encode()")
     return files
 }
 
+func removeDir(dir: URL) throws {
+    print("removeDir(\(dir.path))")
+    // TODO remove dir instead once I'm confident
+    let destinationDir = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("ss_rm")
+    let destinationURL = destinationDir.appendingPathComponent(dir.lastPathComponent)
+
+    if !FileManager.default.fileExists(atPath: destinationDir.path) {
+        try FileManager.default.createDirectory(at: destinationDir, withIntermediateDirectories: true, attributes: nil)
+    }
+
+    try FileManager.default.moveItem(at: dir, to: destinationURL)
+    print("end removeDir()")
+}
+
 func makeMP4s() throws {
-    let files = try encode(blockLength: 300)
+    print("makeMP4s()")
+    // TODO iterate through files in numeric order of keys
+    let files = try encode()
+
+    let tempFile = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+    guard FileManager.default.createFile(atPath: tempFile.path, contents: nil, attributes: nil) else {
+        return // TODO throw
+    }
+    let fileHandle = try FileHandle(forWritingTo: tempFile)
+    // let fileHandle: FileHandle
+    // do {
+    //     fileHandle = try FileHandle(forWritingTo: tempFile)
+    // } catch {
+    //     throw NSError(domain: "FileHandling", code: 1,
+    //         userInfo: [NSLocalizedDescriptionKey: "Cannot create file handle: \(error.localizedDescription)"])
+    // }
 
     for (key, dirs) in files {
+        if !isOnPower {
+            break
+        }
+    
         let mp4Full = fullDir.appendingPathComponent("\(key).mp4")
         let mp4Small = smallDir.appendingPathComponent("\(key).mp4")
 
@@ -88,11 +127,11 @@ func makeMP4s() throws {
             continue
         }
 
-        let tempFile = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
         var concatContent = ""
         for dir in dirs {
             concatContent += "file '\(dir.appendingPathComponent("image.png").path)'\nduration 1.0\n"
         }
+        // TODO maybe use fileHandle
         try concatContent.write(to: tempFile, atomically: true, encoding: .utf8)
 
         let processF = Process()
@@ -127,21 +166,61 @@ func makeMP4s() throws {
             mp4Small.path
         ]
 
-        try processF.run()
-        try processS.run()
+        do {
+            print("running \(processF)")
+            try processF.run()
+            print("running \(processS)")
+            try processS.run()
+        } catch {
+            print("Error: \(error)")
+            continue
+        }
 
         processF.waitUntilExit()
         processS.waitUntilExit()
 
-        // TODO truncate instead of removing entirely
-        try FileManager.default.removeItem(at: tempFile)
+        try fileHandle.truncate(atOffset: 0)
+
+        for dir in dirs {
+            do {
+                try removeDir(dir: dir)
+            } catch {
+                print("Error: failed to remove dir \(dir.path): \(error)")
+            }
+        }
 
         print("made \(key).mp4")
     }
+    // TODO use defer
+    try fileHandle.close()
+    try FileManager.default.removeItem(at: tempFile)
+    print("end makeMP4s()")
 }
 
-do {
-    try makeMP4s()
-} catch {
-    print("Error: \(error)")
+// NOTE takes a moment to update
+func isDeviceOnPower() -> Bool {
+    let snapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
+    let type = IOPSGetProvidingPowerSourceType(snapshot)
+    guard let type = type else {
+        return false
+    }
+    let type2 = type.takeRetainedValue() as String
+    return type2 == kIOPSACPowerValue
 }
+
+let loop = IOPSNotificationCreateRunLoopSource({ _ in
+    isOnPower = isDeviceOnPower()
+
+    if isOnPower && !isProcessing {
+        isProcessing = true
+        do {
+            try makeMP4s()
+        } catch {
+            print("Error: \(error)")
+        }
+        isProcessing = false
+    }
+}, nil).takeRetainedValue() as CFRunLoopSource
+CFRunLoopAddSource(CFRunLoopGetCurrent(), loop, .defaultMode)
+
+RunLoop.current.run()
