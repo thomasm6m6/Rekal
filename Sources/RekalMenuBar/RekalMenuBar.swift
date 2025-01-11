5912ac8d765c54b7193cbd71c316150cd69cc87d
Thomas M <thomasm6m6@gmail.com>
2025-01-11 10:10:32 -0800

    More stubby

--- Sources/RekalMenuBar/RekalMenuBar.swift
+++ Sources/RekalMenuBar/RekalMenuBar.swift
@@ -1,11 +1,23 @@
 import SwiftUI
 
+// TODO XPC with daemon
+
 @main
-struct RekalMenuBar: App {    
+struct RekalMenuBar: App {
+    @State var recording = true
+
     var body: some Scene {
-        MenuBarExtra("Rekal", systemImage: "arrowtriangle.backward.fill") {
+        MenuBarExtra("Rekal", systemImage: recording ? "arrowtriangle.backward.fill" : "arrowtriangle.backward") {
+            Button(recording ? "Pause recording" : "Resume recording") {
+                recording = !recording
+                print(recording ? "recording paused" : "recording resumed")
+            }
+            Button("Process now") {
+                print("process now")
+            }
             Button("Quit") {
                 NSApplication.shared.terminate(nil)
+                // should this also quit the daemon? what about the GUI?
             }.keyboardShortcut("q")
         }
     }
